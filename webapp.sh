9d01e6ecc630ed0f09a50a70d63467641a992a24
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2025-05-13 11:24:26 +0530

    Delete webapp.sh

--- webapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum install git nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo git clone https://github.com/bhargavibairagoni/Brainwave_Matrix_Intern.git
-sudo mv Brainwave_Matrix_Intern/* /usr/share/nginx/html
-sudo systemctl restart nginx
